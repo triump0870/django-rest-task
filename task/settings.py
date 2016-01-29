563293b69237ca49e59635684ea6a866a24a387a
Ubuntu <ubuntu@ip-172-31-27-32.ap-northeast-1.compute.internal>
2016-01-29 15:59:25 +0000

    updated AWS key

--- task/settings.py
+++ task/settings.py
@@ -33,7 +33,10 @@ if not DEBUG:
    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
    STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
    S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-   STATIC_URL = S3_URL# Application definition
+   STATIC_URL = S3_URL
+   STATIC_ROOT = 'staticfiles'
+
+# Application definition
 
 INSTALLED_APPS = (
     'django_admin_bootstrapped',
