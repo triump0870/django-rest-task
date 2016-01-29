5e96d733aef32457c45fdd690f829111636fde9d
Ubuntu <ubuntu@ip-172-31-27-32.ap-northeast-1.compute.internal>
2016-01-29 16:27:32 +0000

    Static files added

--- task/settings.py
+++ task/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'task.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-if not DEBUG:
+if DEBUG:
   DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
